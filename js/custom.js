f9fe801d637e75105338a1ba8e3d72f693bb3fdf
jahangeercs123 <jahangeerahmad26@gmail.com>
2023-03-15 18:00:06 +0500

    3rd commit

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-    $( ".header-buttom" ).slideUp()
     $( ".icon-one" ).click(function(){
         $(this).toggleClass('active-one');
         $( ".header-buttom" ).slideToggle();
       });
   })
-
